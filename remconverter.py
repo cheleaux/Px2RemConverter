b797eafa624e18e798383a4ca580568cfaafc12a
cheleaux <nazareth.dnd@gmail.com>
2024-04-06 11:38:13 +0100

    added error handling and relative path arguement resolver

--- remconverter.py
+++ remconverter.py
@@ -1,7 +1,9 @@
 import re
 import sys
+from os import chdir
 from os.path import abspath, isfile
 from  pathlib import Path
+import logging
 
 
 def pixelsToRem( matchObj ):
@@ -11,26 +13,30 @@ def pixelsToRem( matchObj ):
     remValue = str(remValueInt) + 'rem'
     return remValue
 
-
 arguments = sys.argv[1:]
 
-print( arguments )
 
 # Sort user and bash inserted arguments
 if len( arguments ) != 2 :
-    print(f'TypeError: 1 argument expected. { min( 0, len( arguments ) - 1 ) } given.')
+    raise TypeError(f'1 argument expected. { min( 0, len( arguments ) - 1 ) } given.')
 else:
     filePath = arguments[0]
     workingDirectory = arguments[1]
-    file = filePath if filePath.count('/') > 1 else ( workingDirectory + filePath )
+
+
+# Change directory and resolve path if it's a relative path
+chdir( workingDirectory )
+file = abspath( filePath ) if './' in filePath or '/c/' in filePath or 'C:' in filePath else abspath( './' + filePath )
+
 
 # Check that the file exists and is operable
 if isfile( file ):
     fileName = Path( file ).stem
-    print(f'Conversion unit values in { fileName }...')
+    extName = Path( file ).suffix
+    print(f'Converting unit values in { fileName + extName } ...')
 else:
-    print(f'Error: { file } does is not a file or does not exist')
-    exit()
+    raise FileNotFoundError(f'{ file } is not a file, is a directory or does not exist')
+
 
 # Open the file and write to it with newly converter unit values
 print('running pixel-rem unit conversion...')
@@ -41,6 +47,7 @@ with open( file, 'r+' ) as fileOpen:
         fileOpen.write( re.sub( r'-*\d+px', pixelsToRem, string ))
         fileOpen.truncate()
         print(f'{ fileName } has been successfully converted to a Rem unit file.')
-    except( OSError, IOError ):
-        print('There was an error writing to the file')
-        print( OSError ) if OSError else print( IOError )
+    except( OSError, IOError ) as error:
+        logger = logging.getLogger(__name__)
+        logger.error( error )
+        raise
